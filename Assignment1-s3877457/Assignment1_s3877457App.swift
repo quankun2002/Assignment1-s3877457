e8c403568303e02706455051f1fd51e0c3cee3b4
quankun2002 <s3877457@rmit.edu.vn>
2023-07-28 11:06:19 +0700

    Initial Commit

---
+++ Assignment1-s3877457/Assignment1_s3877457App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1_s3877457App.swift
+//  Assignment1-s3877457
+//
+//  Created by Ashley on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1_s3877457App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment1-s3877457/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Assignment1-s3877457
+//
+//  Created by Ashley on 28/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
